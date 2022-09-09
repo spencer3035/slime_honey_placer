e8248096be2ec2e3516363ff52e599d621253cd8
Spencer <spencer3035@gmail.com>
2022-09-09 16:29:14 -0600

    Added lots of template stuff for a covering algo

--- SlimeHoneyPlacer.java
+++ SlimeHoneyPlacer.java
@@ -3,30 +3,119 @@
 
 // For random numbers
 import java.lang.Math;
+import java.util.ArrayList;
  
 class SlimeHoneyPlacer
 {
+    class Coord {
+        int x;
+        int y;
+    }
+
+    class LineInfo {
+        boolean leftRight;
+        Coord topLeftCoord;
+        Coord bottomRightCoord;
+    }
+    
     static final int MAX_SEGMENT_SIZE = 12;
     static final int MIN_LINE_LENGTH = 3;
 
-    // What values of the grid correspond to
+    // Gotten from ilmango video including 1 space around perimeter
+    static final int HEIGHT = 17;
+    static final int WIDTH = 17;
+
+    // What values of the grid correspond to. 
+    // TODO: Change these to enumes or something else.
     static final int CAN_COVER = 0;
     static final int NEED_TO_COVER = 1;
     static final int CANT_COVER = 2;
     static final int SLIME = 3;
     static final int HONEY = 4;
+    static final int NOT_COVERED = 5;
 
     public static void main(String args[])
     {
-        // Gotten from ilmango video including 1 space around perimeter
-        int h = 17;
-        int w = 17;
-        int[][] grid = genTestGrid(h,w);
-        prettyPrintGrid(grid, h, w);
+        int HEIGHT = 17;
+        int WIDTH = 17;
+        int[][] grid = genTestGrid();
+        prettyPrintGrid(grid);
+    }
+
+    public int[][] genCover(int[][] grid) {
+        int[][] slimeHoneyGrid = new int[HEIGHT][WIDTH];
+
+        // Init to NOT_COVERED
+        for ( int i = 0 ; i < HEIGHT ; i++ ) {
+            for ( int j = 0 ; j < WIDTH ; j++ ) {
+                slimeHoneyGrid[i][j] = NOT_COVERED;
+            }
+        }
+
+        // IDEA: For each block that isn't covered that should be, try to find
+        // the direction that is most locally most lengthy. Then fill in the
+        // direction of that.
+
+
+        // We should randomly seed this a few times before doing the brute force
+        // search to increase odds of finding a good solution.
+        for ( int i = 0 ; i < HEIGHT ; i++ ) {
+            for ( int j = 0 ; j < WIDTH ; j++ ) {
+                if ( slimeHoneyGrid[i][j] == NOT_COVERED && grid[i][j] == NEED_TO_COVER) {
+                    slimeHoneyGrid = greedyLocalCover(i,j,grid,slimeHoneyGrid);
+                }
+            }
+        }
+
+        return slimeHoneyGrid;
+    }
+
+    // Returns the new slimeGrid array.
+    public int[][] greedyLocalCover(Coord pos, int[][] grid, int[][] slimeGrid) {
+        // Try to find MIN_LINE_LENGTH segment horizontally and then virtically
+        // then choose the one that is "better" by some metric. 
+
+        int x = pos.x;
+        int y = pos.y;
+        slimeGrid[x][y] = SLIME;
+        int totalCovered = 1;
+        boolean breakCondition = false;
+
+        // Get max length up-down segment 
+        while ( totalCovered <= 12 && !breakCondition ) {
+            if ( inBounds(x + 1,y) ) {
+
+            } else if ( inBounds(x - 1,y) ) {
+
+            }
+        }
+
+        // Get max length left-right segment 
+        while ( totalCovered <= 12 && !breakCondition ) {
+            if ( inBounds(x + 1,y) ) {
+
+            } else if ( inBounds(x - 1,y) ) {
+
+            }
+        }
+
+        return slimeGrid;
+    }
+
+    public int getLineInfo(Coord pos, int[][] grid, int[][] slimeGrid, boolean leftRight) {
+        int x = pos.x;
+        int y = pos.y;
+        // How many neutral squares to consider before quitting
+        int allowedNeutral = 2;
+        int count
+    }
+
+    public boolean inBounds(int x, int y) {
+        return (x >=0 && x < WIDTH && y >= 0 && y < HEIGHT);
     }
 
 
-    public static void prettyPrintGrid(int[][] grid, int h, int w) {
+    public static void prettyPrintGrid(int[][] grid) {
         String ESC = "\u001b[";
         String block = "██";
         String black    = ESC + "0;30m";
@@ -41,8 +130,8 @@ class SlimeHoneyPlacer
         String colorNeutral = reset + block + reset;
         String colorSlime = green + block + reset;
         String colorHoney = yellow + block + reset;
-        for ( int i = 0 ; i < h ; i++ ) {
-            for ( int j = 0 ; j < h ; j++ ) {
+        for ( int i = 0 ; i < HEIGHT ; i++ ) {
+            for ( int j = 0 ; j < WIDTH ; j++ ) {
                 switch(grid[i][j]) {
                     case CAN_COVER:
                         System.out.print(colorNeutral);
@@ -67,17 +156,17 @@ class SlimeHoneyPlacer
         }
     }
 
-    public static int[][] genTestGrid(int h, int w) {
-        int[][] grid = new int[h][w];
+    public static int[][] genTestGrid() {
+        int[][] grid = new int[HEIGHT][WIDTH];
         double randVal;
 
         // 0: Doesn't matter
         // 1: Needs to be covered
         // 2: Can't be covered
-        for ( int i = 0 ; i < h ; i++ ) {
-            for ( int j = 0 ; j < h ; j++ ) {
+        for ( int i = 0 ; i < HEIGHT ; i++ ) {
+            for ( int j = 0 ; j < WIDTH ; j++ ) {
                 // Makes the shape more circular. Tuned to look kind of right
-                if ( inSquare(i,j,h,w) ) {
+                if ( inSquare(i,j) ) {
                     randVal = Math.random();
                     if ( randVal < 0.1 ) {
                         // Doesn't matter
@@ -100,10 +189,10 @@ class SlimeHoneyPlacer
     }
 
     // Takes in i and j and returns false if they are outside square center
-    private static boolean inSquare(int i, int j, int h, int w) {
+    private static boolean inSquare(int i, int j) {
         // We do minus 2 here to ensure the boundary is always free to place
-        double centerX = (w-2) / 2.0;
-        double centerY = (h-2) / 2.0;
+        double centerX = (WIDTH-2) / 2.0;
+        double centerY = (HEIGHT-2) / 2.0;
         double radius = Math.min(centerX, centerY);
 
         if ( Math.abs(i - centerX) + Math.abs(j - centerY) > radius ) {
